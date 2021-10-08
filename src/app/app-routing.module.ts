7a629e4e97b43f9005d05bdf500d2a952ac852b9
abdulwahab alshehri <abdul.2020alsh@gmail.com>
2021-10-08 18:16:13 +0300

    add authGuard for urls access to pages without authiticated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthCompnent } from './auth/auth.component';
+import { AuhtGuard } from './auth/auth.guard';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
@@ -10,7 +11,10 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const appRouters: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {path: 'recipes', component: RecipesComponent, children: [
+  {path: 'recipes', 
+  component: RecipesComponent,
+  canActivate: [AuhtGuard],
+  children: [
     {path: '', component: RecipeStartComponent },
     {path: 'new', component: RecipeEditComponent },
     {path: ':id', component: RecipeDetailComponent, resolve: [ResipesResolverService] },
